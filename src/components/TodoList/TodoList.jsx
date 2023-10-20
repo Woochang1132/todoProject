1b2d09838fa25fa4cc3a33e6b07ff514e642b2df
Woochang1132 <show4031132@gmail.com>
2023-10-20 16:41:31 +0900

    최우창 : react_todoProject 생성

---
+++ src/components/TodoList/TodoList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function TodoList() {
+    return (
+        <div>
+            <ul>
+                <li>1. 할 리스트 정리</li>
+                <li>2. 정리해야 할 요소정리</li>
+            </ul>
+        </div>
+    );
+}
+
