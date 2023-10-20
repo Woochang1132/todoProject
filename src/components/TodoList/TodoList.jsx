87cbc759568b9db8a98455433cdbbdcfa238e4d0
Woochang1132 <show4031132@gmail.com>
2023-10-20 17:36:57 +0900

    최우창 react_ todoLIst 추가하기 기능

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
+import AddTodo from '../AddTodo/AddTodo';
 
 export default function TodoList() {
+    const [todos, setTodos] = useState([
+        {id : '123', text : '장보기', status : 'active'},
+        {id : '124', text : '공부하기', status : 'active'}])
+   
+    //새로운 todo를 todos에 업데이트 해야 함
+    const handleAdd = (todo) => setTodos([...todos, todo])
+       
+
     return (
-        <div>
+        <section>
             <ul>
-                <li>1. 할 리스트 정리</li>
-                <li>2. 정리해야 할 요소정리</li>
+                {
+                    todos.map(item => <li key={item.id}>{item.text}</li>)
+                }
             </ul>
-        </div>
+
+            <AddTodo onAdd={handleAdd}/>
+        </section>
     );
 }
 
