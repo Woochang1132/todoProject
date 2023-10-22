50027a7f397a9f60e4c8499116f5ef04658522cc
Woochang1132 <show4031132@gmail.com>
2023-10-22 23:47:12 +0900

    최우창 react_챌린지 진행중

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -2,7 +2,8 @@ import React, {useState} from 'react';
 import AddTodo from '../AddTodo/AddTodo';
 import Todo from '../Todo/Todo';
 
-export default function TodoList() {
+
+export default function TodoList({filter}) {
     const [todos, setTodo] = useState([
         {id : '1' , text : "react 공부하기", status : 'active'},
         {id : '2' , text : "todoList 채우가", status : 'active'},
@@ -10,22 +11,30 @@ export default function TodoList() {
     ]) 
 
     const handleAdd = ((todo) => setTodo([...todos, todo]));
-    const handleUpdate = (updated) => 
-    setTodo(todos.map((t) => t.id === updated.id ? updated : t ));
+    const handleUpdate = (updated) =>
+    setTodo(todos.map((t) => t.id === updated.id ? updated : t));
 
     const handleDelete = (deleted) => setTodo(todos.filter((t) => t.id !== deleted.id))
 
+    const filtered = getFilteredItems(todos, filter);
     return (
         <section>
             <ul>
-                {todos.map((item) => (
-                    <Todo key = {item.id}
-                    todo={item} onUpdate={handleUpdate} onDelete={handleDelete}>
-                  </Todo>
-                ))}
+                {filtered.map((item) =>
+                <Todo key={item.id}
+                todo = {item}
+                onUpdate={handleUpdate}
+                onDelete={handleDelete}>
+                </Todo>)}
             </ul>
             <AddTodo onAdd={handleAdd}/>
         </section>
     );
 }
 
+function getFilteredItems(todos, filter){
+    if(filter === 'all'){
+        return todos;
+    }
+    return todos.filter(todo => todo.status === filter);
+}
